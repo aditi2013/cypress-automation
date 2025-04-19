088affff7287f8a493148cfd243d98891c51eacf
jaydip <jaydip10@gmail.com>
2025-04-19 23:28:53 +0530

    Use of fixtures

--- cypress/integration/examples/EndToEndECommerceTest.js
+++ cypress/integration/examples/EndToEndECommerceTest.js
@@ -1,16 +1,36 @@
-describe('End to end ecommerce test', function(){
-    it('Submit order test', function(){
-        const productName="Nokia Edge";
+describe('End to end ecommerce test', function () {
+
+    before(function () {
+        cy.fixture("example").then(function (data) {
+            this.data = data;
+        })
+    });
+
+    it('Submit order test', function () {
+        const productName = this.data.productName;
         cy.visit('https://rahulshettyacademy.com/loginpagePractise/#/');
-        cy.get('#username').type('rahulshettyacademy');
-        cy.get('#password').type('learning');
+        cy.get('#username').type(this.data.username);
+        cy.get('#password').type(this.data.password);
         cy.contains("Sign In").click();
         cy.contains('Shop Name').should('be.visible');
         cy.get('app-card').should('have.length', 4);
-        cy.get('app-card').filter(`:contains("${productName}")`).then($el=>{
-            cy.wrap($el).contains('button','Add').click();
+        cy.get('app-card').filter(`:contains("${productName}")`).then($el => {
+            cy.wrap($el).contains('button', 'Add').click();
         });
-        cy.get('app-card').eq(0).contains('button','Add').click();
+        cy.get('app-card').eq(0).contains('button', 'Add').click();
         cy.contains('a', 'Checkout').click();
+        let sum = 0;
+        cy.get('tr td:nth-child(4) strong').each($el => {
+            const amount = Number($el.text().split(" ")[1].trim());
+            sum = sum + amount;
+        }).then(function () {
+            expect(sum).to.be.lessThan(200000);
+        });
+        cy.contains('button', 'Checkout').click();
+        cy.get('#country').type("India");
+        cy.wait(2000);
+        cy.get('.suggestions ul li a').click();
+        cy.get('.btn-success').click();
+        cy.get('.alert-success').should('contain', 'Success');
     });
 });
